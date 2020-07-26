8103ea83bc3cf0476c72228d3bcc2e265d73f91d
Gui.go <guilhermeviegas1993@gmail.com>
2020-07-26 00:50:41 -0300

    added a few Shiny scripts and organized stuff

---
+++ R/ShinyTemplates/OldFaithfullGeyserShinyApp.r
@@ -0,0 +1,37 @@
+# R script - Simplest Shiny App
+
+# Setup -------------------------------------------------------------------
+# rm(list = ls())
+gc()
+options(encoding = "UTF-8", stringsAsFactors = F)
+
+# UI ----------------------------------------------------------------------
+ui <- fluidPage(
+  titlePanel("Hello Shiny!"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(inputId = "bins",
+                  label = "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    mainPanel(
+      plotOutput(outputId = "distPlot")
+    )
+  )
+)
+
+# Server ------------------------------------------------------------------
+server <- function(input, output) {
+  output$distPlot <- renderPlot({
+    x    <- faithful$waiting
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    hist(x, breaks = bins, col = "#75AADB", border = "white",
+         xlab = "Waiting time to next eruption (in mins)",
+         main = "Histogram of waiting times")
+  })
+}
+
+# App ---------------------------------------------------------------------
+shinyApp(ui, server)
