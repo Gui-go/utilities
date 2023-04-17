1d99a5610891ba2cf1a979939ab7c95ac6d886f5
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2023-04-18 00:57:40 +0100

    Create help_utilities.py

---
+++ Spotfire/help/help_utilities.py
@@ -0,0 +1,7 @@
+#print("Document Data Filterings: ---------------------")
+#for f in Document.Data.Filterings:
+#	print f.Name
+#print("Document Properties: ---------------------")
+#for propertyName in Document.Properties:
+#    print(propertyName.Name)
+#print(list(Document.Properties['SelectColumns']))
