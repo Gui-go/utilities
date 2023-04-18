a1f332da24d4d2297ebe3d9710aa93de941d52ad
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2023-04-18 15:00:47 +0100

    Update help_utilities.py

--- Spotfire/help/help_utilities.py
+++ Spotfire/help/help_utilities.py
@@ -2,6 +2,9 @@
 #for f in Document.Data.Filterings:
 #	print f.Name
 #print("Document Properties: ---------------------")
-#for propertyName in Document.Properties:
-#    print(propertyName.Name)
+#for g in Document.Properties:
+#    print(g.Name)
 #print(list(Document.Properties['SelectColumns']))
+#print("Document Data Tables: ---------------------")
+#for h in Document.Data.Tables:
+#	print(h.Name)
